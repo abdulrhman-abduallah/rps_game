a6a507cfef4baf91dccd2841692ec0035e403f23
Abdulrhman Idres <abdlrhman_abdallah@outlook.com>
2024-10-22 07:57:22 +0400

    first steps in the project

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
 // The first project to actually create on git
 console.log("Hello, Rayan! I love you.");
+
+function getComputerChoice(max) {
+	return Math.floor(Math.random() * max);
+
+}
+
+
+function getHumanChoice() {
+	let choice = prompt("Please enter your choice: ");
+}
+getHumanChoice();
